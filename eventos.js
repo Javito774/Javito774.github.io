9f282a643006ee49d6f38c5b2b2e7cd28e2fa37e
Javito774 <javito774@gamil.com>
2017-10-07 19:58:26 +0200

    Retoques de cabecera

--- eventos.js
+++ eventos.js
@@ -4,7 +4,6 @@ window.onscroll = function(){
 	if(wScroll<300)
 	{
 		document.querySelector('.logoNombre').style.transform="translate(0px, "+ wScroll/2 +"%)";
-		document.querySelector('.lineaLogo>hr').style.transform="rotate(-"+ (wScroll+90) +"deg)";
 
 		document.querySelector('.slogan1').style.transform="translate(0px,-"+wScroll+"px)";
 		document.querySelector('.slogan2').style.transform="translate(0px,-"+wScroll+"px)";
