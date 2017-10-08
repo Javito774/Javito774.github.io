9f44f41c59f5a675b7b48b555a7579aba235681c
Javito774 <javito774@gamil.com>
2017-10-08 11:07:43 +0200

    Añado el About Me

--- eventos.js
+++ eventos.js
@@ -10,4 +10,24 @@ window.onscroll = function(){
 		document.querySelector('.slogan3').style.transform="translate(0px,-"+wScroll+"px)";
 		document.querySelector('.slogan4').style.transform="translate(0px,-"+wScroll+"px)";
 	}
+	if(wScroll<500)
+	{
+		document.querySelector('#aboutMe h3').style.transform="translate(-"+wScroll/8+"px,0px)";
+		document.querySelector('#aboutMe p').style.transform="translate("+wScroll/8+"px,0px)";
+	}
 };
+window.onload = function()
+{
+	setTimeout(function(){
+		document.querySelector('#aboutMe h3').style.transform="translate(0px,0px)";
+		document.querySelector('#aboutMe h3').style.opacity="1";
+		setTimeout(function() {
+			document.querySelector('#aboutMe p').style.transform="translate(0px,0px)";
+			document.querySelector('#aboutMe p').style.opacity="1";
+			setTimeout(function(){
+				document.querySelector('#aboutMe h3').style.transition="none";
+				document.querySelector('#aboutMe p').style.transition="none";
+			},500);
+		},200);
+	},500);
+}
