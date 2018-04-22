b1abc71c438574b9f957e867a154ed534840c274
javito774 <javito774@gmail.com>
2018-04-22 22:07:08 +0200

    Segunda correccion de errores

--- scripts/general.js
+++ scripts/general.js
@@ -1,6 +1,10 @@
 if (screen.width<1024)
 {
     document.querySelector("main>.bloque>article>h2").style.paddingLeft="10%";
-    document.querySelectorAll("main>.bloque>article>div>p").style.width="80%";
-    document.querySelectorAll("main>.bloque>article>div>p").style.margin="0 auto";
+    var parrafos = document.querySelectorAll("main>.bloque>article>div>p")
+    for(var i=0;i<parrafos.length;i++)
+    {
+    	parrafos[i].style.width="80%";
+    	parrafos[i].style.margin="0 auto";
+    }
 }
