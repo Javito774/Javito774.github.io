ccd2cd742b6056392fbca9839897d4c99e644e77
javito774 <javito774@gmail.com>
2018-04-22 22:12:01 +0200

    Correccion tercera

--- scripts/general.js
+++ scripts/general.js
@@ -1,7 +1,7 @@
 if (screen.width<1024)
 {
     document.querySelector("main>.bloque>article>h2").style.paddingLeft="10%";
-    var parrafos = document.querySelectorAll("main>.bloque>article>div>p")
+    var parrafos = document.querySelectorAll("main>.bloque>article>div>p");
     for(var i=0;i<parrafos.length;i++)
     {
     	parrafos[i].style.width="80%";
